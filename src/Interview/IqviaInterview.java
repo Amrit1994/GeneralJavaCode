6ff1493324f15ac1beeb487c1d767a6f5fb9d43c
Amrit1994 <jhaamrit.nr@gmail.com>
2024-09-05 22:28:10 +0530

    Added question and removed warnings

--- src/Interview/IqviaInterview.java
+++ src/Interview/IqviaInterview.java
@@ -1,19 +1,21 @@
 package Interview;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class IqviaInterview {
 
     public static void main(String[] args) {
+        // get max repeated count element
         //{2,3,1,4,4,1,4,333,3,333,2,2,2,5,222};
         // expected 2
         int[] arr = {2,3,1,4,4,1,4,333,3,333,2,2,2,5,222};
-       Optional<Integer> value =  Arrays.stream(arr).mapToObj(e->e).collect(Collectors.groupingBy(e-> e, Collectors.counting()))
-                .entrySet().stream().sorted((e1, e2) -> Integer.compare(Integer.valueOf(e2.getValue().toString()), Integer.valueOf(e1.getValue().toString())))
-                .map(e -> e.getKey()).findFirst();
+       Optional<Integer> value =  Arrays.stream(arr).boxed().collect(Collectors.groupingBy(e-> e, Collectors.counting()))
+                .entrySet().stream().sorted((e1, e2) -> Integer.compare(Integer.parseInt(e2.getValue().toString()), Integer.parseInt(e1.getValue().toString())))
+                .map(Map.Entry::getKey).findFirst();
 
-        System.out.println(value.orElse(null));
+        System.out.println(value.orElse(null)); // 2
     }
 }
